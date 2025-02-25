5d8ec5ecb4bc341c21c9619376b6eed66d886cf2
Daniele Caldana <danielecaldana02@gmail.com>
2025-02-25 18:25:25 +0000

    added things

---
+++ Makefile
@@ -0,0 +1,2 @@
+documentation:
+	cd docs && doxygen
---
+++ include/example.h
@@ -0,0 +1,31 @@
+/**
+ * @file example.h
+ * @version 1.0
+ * @date 2021-09-30
+ * @brief Example header file
+ * @authors Eu, Eu2
+ */
+
+
+
+/**
+ * @brief Something...
+ * 
+ * @param[in] a This is a string
+ * @param[out ] b This is an integer
+ * @return int 
+ */
+ int foo( char * a, int b);
+
+ 
+
+ /**
+  * @brief Struct...
+  * 
+  * @param a This is an integer
+  * @param b This is an integer
+  */
+ typedef struct {
+     int a; //!< This is another comment
+     int b; //!< This is a comment
+ } my_struct_t;
